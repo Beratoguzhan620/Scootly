ec53260066d907257f359a9f73fa178a4a777720
Berat Oğuzhan Turğut <beratoguzhan620@gmail.com>
2026-08-22 21:04:47 +0300

    feat(domain): add vehicle aggregate with status transitions

---
+++ src/Scootly.Domain/Fleet/VehicleId.cs
@@ -0,0 +1,20 @@
+﻿using Scootly.Domain.Common;
+
+namespace Scootly.Domain.Fleet;
+
+public sealed class VehicleId : ValueObject
+{
+    public Guid Value { get; }
+
+    public VehicleId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static VehicleId New() => new(Guid.NewGuid());
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
---
+++ src/Scootly.Domain/Fleet/VehicleModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Scootly.Domain.Fleet;
+
+public sealed class VehicleModel
+{
+    public string Brand { get; }
+    public int RangeKm { get; }
+
+    public VehicleModel(string brand, int rangeKm)
+    {
+        Brand = brand;
+        RangeKm = rangeKm;
+    }
+}
