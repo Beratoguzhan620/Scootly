1dd35b65cd0ca880905e0fb87b666d918a91f0fc
Berat Oğuzhan Turğut <beratoguzhan620@gmail.com>
2026-08-31 21:27:49 +0300

    test(api): add integration test infrastructure with Testcontainers

---
+++ tests/Scootly.Api.IntegrationTests/SmokeTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Scootly.Api.IntegrationTests;
+
+public sealed class SmokeTests : IClassFixture<ScootlyApiFactory>
+{
+    private readonly ScootlyApiFactory _factory;
+
+    public SmokeTests(ScootlyApiFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Api_Ayaga_Kalkip_Vehicles_Ucuna_Yanit_Vermeli()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/api/vehicles");
+
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ tests/Scootly.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Scootly.Api.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
